fe8fd19ec5c4b675e1cb26e866a4845509cee067
ShristiA <shristiamatya3@gmail.com>
2018-10-05 19:26:39 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+import java.util.Scanner;
+// to get the imput from user
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner src = new Scanner(System.in);
+    //to initiate user's input
+        String ans = " ";
+do {
+    System.out.println("enter you number");
+    int num = src.nextInt();
+    //asking and storing user's number in num variable.
+
+    do {
+
+        if (num % 5 == 0 || num % 7 == 0) {
+            System.out.println(num);
+          }
+
+        num = num - 1;
+    } while (num >= 1);
+    System.out.println("enter another number? (y/n)");
+    ans =  src.next();
+}while( ans .equals("y"));
+        if (ans .equals("n")) {
+            System.out.print( 5%2 );
+        }}
+
+}
